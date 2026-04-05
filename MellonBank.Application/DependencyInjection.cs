86baf08918a02fc65e29e73ea65caaf0c9a820e3
Akis <klakrivos@gmail.com>
2026-04-05 20:39:47 +0300

    folder structure

---
+++ MellonBank.Application/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MellonBank.Application
+{
+    internal class DependencyInjection
+    {
+    }
+}
---
+++ MellonBank.Infrastructure/Persistence/MellonBankDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MellonBank.Infrastructure.Persistence
+{
+    internal class MellonBankDbContext
+    {
+    }
+}

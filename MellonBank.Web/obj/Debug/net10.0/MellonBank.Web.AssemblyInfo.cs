b2643b4ad6578fb7f3d0527f0e39a7946318f404
Akis <klakrivos@gmail.com>
2026-04-16 13:51:31 +0300

    Add readme file

--- MellonBank.Web/obj/Debug/net10.0/MellonBank.Web.AssemblyInfo.cs
+++ MellonBank.Web/obj/Debug/net10.0/MellonBank.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MellonBank.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380dcae712ea2d2db0ce619af74760d415975507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ae70b8221a83c4182f1675492fcb5f4ad6633d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MellonBank.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MellonBank.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
